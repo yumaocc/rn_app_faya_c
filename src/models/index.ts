5f0c51b79ca527f9678042ea063d0f003ee87b53
simon <simon@sayto.top>
2022-09-07 12:47:24 +0800

    feat: 钱包相关model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,11 @@
 export type {RootStackParamList, Props, FakeNavigation} from './route';
-export type {UserInfo} from './user';
+export type {UserInfo, CouponF, WalletInfo} from './user';
 export type {AppHeader, StylePropText, StylePropView, CacheKeys} from './common';
 export type {WorkTab, WorkF, WorkDetailF} from './work';
 export type {SPUF, SPUShop, PackageDetail, SKUDetail, SPUDetailF} from './spu';
 export type {OrderF, OrderDetailF, OrderPackageSKU} from './order';
 
-export {UserState} from './user';
+export {UserState, CouponState, CouponType} from './user';
 export {WorkTabType, WorkType} from './work';
 export {SPUStatus, SPUType, BookingType, PayChannel} from './spu';
 export {OrderStatus} from './order';
--- src/models/user.ts
+++ src/models/user.ts
@@ -9,3 +9,37 @@ export interface UserInfo {
   status: UserState;
   token: string;
 }
+
+export enum CouponState {
+  Unused = 0,
+  Used = 1,
+  InvalidByRefund = 2,
+  InvalidByDirect = 3,
+}
+
+export enum CouponType {
+  NoLimit = 0,
+}
+
+export interface CouponF {
+  id: number;
+  bindUserId: number;
+  bindUserTime: string; // 领取时间
+  amountThreshold: number; // 满多少可用
+  createdByCom: number; // 创建人
+  createdTime: string;
+  justInvalidTime: string; // 直接作废时间
+  justInvalidUserIdCom: number; // 操作直接作废的公司员工
+  money: number; // 优惠券金额
+  name: string; // 优惠券名字
+  refundInvalidTime: string; // 退款作废时间
+  status: CouponState;
+  type: CouponType;
+  usedMoney: number; // 已使用金额
+  usedTime: string; // 使用时间
+}
+
+export interface WalletInfo {
+  money: number;
+  numberOfCards: number;
+}
