0dae8986be78ecfa296c90a239343fd2e02f74c5
Simon <simon@sayto.top>
2022-10-10 21:29:20 +0800

    feat: 银行卡列表

--- src/screen/mine/wallet/BankCards.tsx
+++ src/screen/mine/wallet/BankCards.tsx
@@ -1,10 +1,48 @@
+import {SwipeAction} from '@ant-design/react-native';
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import {View, Text, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import {NavigationBar} from '../../../component';
+import {globalStyles, globalStyleVariables} from '../../../constants/styles';
+import {useBankCards} from '../../../helper/hooks';
 
 const BankCards: React.FC = () => {
+  const [bankCards] = useBankCards();
   return (
     <View style={styles.container}>
-      <Text>BankCards</Text>
+      <NavigationBar title="银行卡管理" />
+      <ScrollView style={{flex: 1}}>
+        {bankCards.map((card, i) => {
+          return (
+            <SwipeAction key={i} right={[{text: '解绑', backgroundColor: '#f00', color: '#fff'}]}>
+              <View style={{paddingHorizontal: globalStyleVariables.MODULE_SPACE}}>
+                <View style={[globalStyles.containerRow, styles.item]}>
+                  <Text>
+                    {card.bankCodeName}（{card.accountNo}）
+                  </Text>
+                </View>
+                <View style={globalStyles.lineHorizontal} />
+              </View>
+            </SwipeAction>
+          );
+        })}
+        {/* <SwipeAction right={[{text: '解绑', backgroundColor: '#f00', color: '#fff'}]}>
+          <View style={{paddingHorizontal: globalStyleVariables.MODULE_SPACE}}>
+            <View style={[globalStyles.containerRow, styles.item]}>
+              <Text>中国工商银行（6228480402564890018）</Text>
+            </View>
+            <View style={globalStyles.lineHorizontal} />
+          </View>
+        </SwipeAction> */}
+        <View style={[styles.item, globalStyles.containerRow, {paddingLeft: globalStyleVariables.MODULE_SPACE}]}>
+          <TouchableOpacity activeOpacity={0.8}>
+            <View style={[globalStyles.containerRow]}>
+              <MaterialIcon name="add" color={globalStyleVariables.COLOR_CASH} size={24} />
+              <Text style={[globalStyles.fontPrimary, {color: globalStyleVariables.COLOR_CASH}]}>添加银行卡</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -14,7 +52,9 @@ export default BankCards;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  item: {
+    height: 50,
   },
 });
