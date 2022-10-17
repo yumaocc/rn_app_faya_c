6a199512c484d6aea9743e69f1fcf45c1682235d
Simon <simon@sayto.top>
2022-10-17 16:44:12 +0800

    feat: lottie动画支持

--- src/screen/common/Loading.tsx
+++ src/screen/common/Loading.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {View, Image, StyleSheet, StatusBar} from 'react-native';
 import {useCommonDispatcher} from '../../helper/hooks';
+// import LottieView from 'lottie-react-native';
 
 const Loading: React.FC = () => {
   const [commonDispatcher] = useCommonDispatcher();
@@ -13,6 +14,7 @@ const Loading: React.FC = () => {
     <View style={styles.container}>
       <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
       <Image source={require('../../assets/slogan_white.png')} style={styles.slogan} />
+      {/* <LottieView style={styles.anim} source={require('../../lotties/loading.json')} autoPlay loop /> */}
     </View>
   );
 };
@@ -21,7 +23,8 @@ export default Loading;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FF7F63',
+    // backgroundColor: '#FF7F63',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -29,5 +32,8 @@ const styles = StyleSheet.create({
     marginTop: -240,
     width: 290,
     height: 130,
+  },
+  anim: {
+    width: 100,
   },
 });
