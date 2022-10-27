4cbadf9f594560468a98c5b4e52fb16821f23116
Simon <simon@sayto.top>
2022-10-27 16:03:55 +0800

    feat: 扫码相机

--- src/screen/common/Scanner.tsx
+++ src/screen/common/Scanner.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
 const Scanner: React.FC = () => {
+  const devices = useCameraDevices();
+  console.log(devices);
   function handleSuccess(e: any) {
     console.log(e);
+  }
+
+  if (!devices.back) {
+    return (
+      <View style={styles.loading}>
+        <Text>loading</Text>
+      </View>
+    );
   }
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
-      <Text>Scanner</Text>
+      <Camera style={{flex: 1}} device={devices.back} isActive />
     </View>
   );
 };
@@ -18,5 +29,10 @@ export default Scanner;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
